f4795d12c16c142287605679f01736d0babb8646
Humberto Hernandez <hamburger45@gmail.com>
2020-02-08 12:00:53 -0800

    added .sql file and sql script

--- src/app.js
+++ src/app.js
@@ -23,9 +23,9 @@ app.get('/', (req, res) => {
 app.use(function validateBearerToken(req, res, next) {
 	const apiToken = process.env.API_TOKEN;
 	const authToken = req.get('Authorization');
-
+	// MAKE SURE TO INCLUDE BEARER!!!!!
 	if (!authToken || authToken.split(' ')[1] !== apiToken) {
-		logger.error(`Unauthorized request to path: ${req.path}`);
+		// logger.error(`Unauthorized request to path: ${req.path}`);
 		return res.status(401).json({ error: 'Unauthorized request' });
 	}
 	next();
